af06d84790f35e20e5ce441441e3bd04ff7d8054
Raidienaty <jcanducci@gmail.com>
2022-06-03 10:15:40 -0400

    Added reference

--- main.py
+++ main.py
@@ -40,6 +40,8 @@ def main():
 
     #matrixDF.head(10)
 
+    # Thank you to this blessing post:
+    # https://stackoverflow.com/questions/61335563/how-to-reorder-of-rows-of-a-dataframe-based-on-values-in-a-column
     order = ['8:00am - 9:15am', '9:30am - 10:45am', '11:00am - 12:15pm', '12:30pm - 1:45pm', '2:00pm - 3:15pm', '3:30pm - 4:45pm', '5:00pm - 6:15pm', '6:30pm - 7:45pm', '8:00pm - 9:15pm']
     matrixDF = matrixDF.reindex(matrixDF['Day'].map(dict(zip(order, range(len(order))))).sort_values().index)
 
